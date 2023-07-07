e9d01de68a987efe8770b967beb79f3839a0d468
SarthakBhave <kanaiya2182@gmail.com>
2023-07-07 21:43:03 +0530

    hero section

--- src/components/tape section/Tape.js
+++ src/components/tape section/Tape.js
@@ -2,17 +2,23 @@ import React from "react";
 import "./tape.css";
 
 const Tape = () => {
-    return (
-        <div className="tape-container">
-
-            <div className="tape-1">
-                <p>When art meets web3 🎨 When art meets web3 🎨 When art meets web3 🎨 When art meets web3 🎨 When art meets  </p>
-            </div>
-            <div className="tape-2">
-                <p>Showcase, Tokenize, and Discover Art as NFTs 🧩 Showcase, Tokenize, and Discover Art as NFTs 🧩 Showcase, Tokenize, and Discover Art as NFTs 🧩</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="tape-container">
+      <div className="tape-1">
+        <p>
+          🎨When art meets web3 🎨 When art meets web3 🎨 When art meets web3 🎨
+          When art meets web3 🎨 When art meets{" "}
+        </p>
+      </div>
+      <div className="tape-2">
+        <p>
+          TS 🧩Showcase, Tokenize, and Discover Art as NFTs 🧩 Showcase,
+          Tokenize, and Discover Art as NFTs 🧩 Showcase, Tokenize, and Discover
+          Art as NFTs 🧩
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Tape;
