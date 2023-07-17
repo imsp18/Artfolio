67624457f5254d40b678a65179adab7702351241
SarthakBhave <kanaiya2182@gmail.com>
2023-07-17 21:15:37 +0530

    finalchecks

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,13 +3,13 @@ import "./navbar.css";
 import { NavLink } from "react-router-dom";
 import * as fcl from "@onflow/fcl";
 import * as t from "@onflow/types";
-import { useEffect, useState } from 'react';
-import logout from './assets/log-out.svg';
+import { useEffect, useState } from "react";
+import logout from "./assets/log-out.svg";
 
-fcl.config()
+fcl
+  .config()
   .put("accessNode.api", "https://access-testnet.onflow.org")
-  .put("discovery.wallet", "https://flow-wallet-testnet.blocto.app/authn")
-
+  .put("discovery.wallet", "https://flow-wallet-testnet.blocto.app/authn");
 
 const Navbar = () => {
   const [user, setUser] = useState();
@@ -32,13 +32,29 @@ const Navbar = () => {
     <nav>
       <div className="logo">Artfolio</div>
       <ul className="menu">
-        <li><NavLink to="/" className="navlink">Home</NavLink></li>
-        <li><NavLink to="/discover" className="navlink">Discover</NavLink></li>
-        <li><NavLink to="/tokenize" className="navlink">Tokenize</NavLink></li>
-        <li><NavLink to="/learn" className="navlink">Learn</NavLink></li>
+        <li>
+          <NavLink to="/" className="navlink">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/discover" className="navlink">
+            Discover
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/tokenize" className="navlink">
+            Tokenize
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/learn" className="navlink">
+            Learn
+          </NavLink>
+        </li>
       </ul>
       <button className="wallet-button" onClick={handleWalletToggle}>
-        {isConnected ? `${user?.addr}` : 'Connect Wallet'}
+        {isConnected ? `${user?.addr}` : "Connect Wallet"}
         {isConnected && (
           <img onClick={handleWalletToggle} src={logout} alt="Log out" />
         )}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,10 @@ import React from "react";
 import Hero from "../components/hero/Hero";
 import Tape from "../components/tape/Tape";
 import About from "../components/about/About";
-import Vision from "../components/vision/vision"; 
+import Vision from "../components/Vision/vision";
 import Mission from "../components/mission/Mission";
-import Feature from "../components/feature/Feature"; 
-import Join from "../components/join/Join"; 
-
-
+import Feature from "../components/feature/Feature";
+import Join from "../components/join/Join";
 
 const App = () => {
   return (
